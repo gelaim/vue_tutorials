144fa0bebe404c7db31a82d1dc4ab12b40960190
Thiago <t.gelaim@gmail.com>
2022-06-09 19:36:24 -0300

    Conditionals: first if.

---
+++ lists-cond-01-starting-setup/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+  data() {
+    return { goals: [], enteredGoalValue: "" };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+    },
+  },
+});
+
+app.mount("#user-goals");
