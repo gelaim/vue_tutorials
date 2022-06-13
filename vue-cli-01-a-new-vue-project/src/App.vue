1904f60f559ff7b89aba14d3a6840d11b87d3d8d
Thiago <t.gelaim@gmail.com>
2022-06-13 17:54:05 -0300

    Vue CLI: a first template.

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lopesz",
+          phone: "4324324",
+          email: "manuel@example.com",
+        },
+        {
+          id: "manuela",
+          name: "Manuela Lopesz",
+          phone: "43243245454",
+          email: "manuela@example.com",
+        },
+      ],
+    };
+  },
+};
+</script>
