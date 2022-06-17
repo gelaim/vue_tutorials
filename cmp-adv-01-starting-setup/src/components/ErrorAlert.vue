72e1871cec3b1df4f1bf6833f14f33c548b90723
Thiago <t.gelaim@gmail.com>
2022-06-17 18:52:15 -0300

    Components: dynamic components with slots and buttons.

---
+++ cmp-adv-01-starting-setup/src/components/ErrorAlert.vue
@@ -0,0 +1,16 @@
+<template>
+  <dialog open>
+    <slot> </slot>
+  </dialog>
+</template>
+<style scoped>
+dialog {
+  margin: 0px;
+  position: fixed;
+  top: 20vh;
+  left: 30%;
+  width: 40%;
+  background-color: white;
+  padding: 1rem;
+}
+</style>
--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,4 +1,30 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text" />
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set goal</button>
+  <ErrorAlert v-if="inputIsInvalid"
+    ><h2>Input is invalid</h2>
+    <button @click="confirmError">Ok</button></ErrorAlert
+  >
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: { ErrorAlert },
+  data() {
+    return { inputIsInvalid: false };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
