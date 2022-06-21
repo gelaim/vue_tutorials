594ae27c8621b26a81f18d8715865c17bbce742b
Thiago <t.gelaim@gmail.com>
2022-06-21 18:44:18 -0300

    Routes: add custom path.

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -3,11 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
-
+import TeamMembers from './components/teams/TeamMembers.vue';
 const router = createRouter({
   routes: [
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
+    { path: '/teams/:teamId', component: TeamMembers },
   ],
   history: createWebHistory(),
 });
