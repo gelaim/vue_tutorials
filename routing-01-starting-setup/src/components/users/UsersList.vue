16d7297a900f0a072e4453ddddafc10376b32060
Thiago <t.gelaim@gmail.com>
2022-06-25 14:24:25 -0300

    Routing: route leave guards avoiding accidently loosing data.

--- routing-01-starting-setup/src/components/users/UsersList.vue
+++ routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save changes</button>
   <ul>
     <user-item
       v-for="user in users"
@@ -18,10 +19,26 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return { changesSaved: false };
+  },
   methods: {
     confirmInput() {
       this.$router.push('/teams');
     },
+    saveChanges() {
+      this.changesSaved = true;
+    },
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('userlistComponente before');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const wants = confirm('Are you sure? You got unsaved changes');
+      next(wants);
+    }
   },
 };
 </script>
