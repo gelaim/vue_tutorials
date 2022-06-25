1409f1236fb533e589c789640aad6f847a6d4e97
Thiago <t.gelaim@gmail.com>
2022-06-25 15:57:57 -0300

    Animation: transitions with css + js, v2

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -60,7 +60,6 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  transition: transform 0.3s ease-out;
 }
 .container {
   max-width: 40rem;
@@ -74,6 +73,19 @@ button:active {
   border-radius: 12px;
 }
 .animate {
-  transform: translateX(-150px);
+  animation: slide-fade 1.3s ease-out forwards;
+}
+@keyframes slide-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
 }
 </style>
