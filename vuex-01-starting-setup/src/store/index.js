c22618d60b1d06a96c52182c2cfa3911eecf4627
Thiago <t.gelaim@gmail.com>
2022-06-26 12:02:26 -0300

    Vuex: struture code.

---
+++ vuex-01-starting-setup/src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex';
+import rootMutations from './mutations.js';
+import rootGetters from './getters.js';
+import rootActions from './actions.js';
+import counterModule from './counter/index.js';
+
+const store = createStore({
+  modules: {
+    numbers: counterModule,
+  },
+  state() {
+    return { isLoggedIn: false };
+  },
+  mutations: rootMutations,
+  getters: rootGetters,
+  actions: rootActions,
+});
+
+export default store;
