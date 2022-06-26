5338bfe2106fa08cb6566b8a9ccb7da38ddff1b4
Thiago <t.gelaim@gmail.com>
2022-06-26 16:20:05 -0300

    Vuex: challenge.

---
+++ vuex-11-a-challenge-starting-code/src/store/index.js
@@ -0,0 +1,35 @@
+import { createStore } from 'vuex';
+import productsModules from './modules/products.js';
+import cartModule from './modules/cart.js';
+const store = createStore({
+  modules: {
+    prods: productsModules,
+    cart: cartModule,
+  },
+  state() {
+    return { isLoggedIn: false };
+  },
+  mutations: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+  actions: {
+    login(context) {
+      context.commit('login');
+    },
+    logout(context) {
+      context.commit('logout');
+    },
+  },
+  getters: {
+    isAuthenticated(state) {
+      return state.isLoggedIn;
+    },
+  },
+});
+
+export default store;
