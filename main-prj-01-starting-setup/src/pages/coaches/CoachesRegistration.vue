0bd6da36dac5aa59ba4816bd671e350ee3fca251
Thiago <t.gelaim@gmail.com>
2022-06-30 19:30:21 -0300

    Main app: create form.

--- main-prj-01-starting-setup/src/pages/coaches/CoachesRegistration.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesRegistration.vue
@@ -1,1 +1,15 @@
-<template>Register</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register a coach now!</h2>
+      <coach-form></coach-form
+    ></base-card>
+  </section>
+</template>
+<script>
+import BaseCard from '../../components/ui/BaseCard.vue';
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: { CoachForm, BaseCard },
+};
+</script>
